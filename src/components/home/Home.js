63d4fdb1c9c92f3dcd87299e9ae547eca69b1cdd
yang052413 <liyang0525.ly@gmail.com>
2020-04-25 14:02:14 -0700

    display user location as map, connect google map api

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react"
 import firebase from "firebase"
+import Map from "./Map"
 
 class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      lat: null,
+      lng: null,
       location: {
         house_number: "",
         road: "",
@@ -17,22 +20,21 @@ class Home extends Component {
   }
   componentDidMount() {
     const db = firebase.firestore()
-
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords.latitude)
-      console.log(position.coords.longitude)
       let token = "a1ee650f1079c4"
       let lat = position.coords.latitude
       let lon = position.coords.longitude
       let url = "https://us1.locationiq.com/v1/reverse.php?"
       let requestUrl = `${url}key=${token}&lat=${lat}&lon=${lon}&format=json`
 
+      //reverse geocoding user geolocation
       fetch(requestUrl)
         .then((res) => res.json())
         .then((location) => {
           this.setState({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
             location: {
-              house_number: location.address.house_number,
               road: location.address.road,
               city: location.address.city,
               state: location.address.state,
@@ -41,13 +43,18 @@ class Home extends Component {
             },
           })
 
+          //write user location to firebase
           firebase.auth().onAuthStateChanged((user) => {
             db.collection("user")
               .doc(user.uid)
               .set(
                 {
                   Location: {
-                    house_number: "dd",
+                    road: location.address.road,
+                    city: location.address.city,
+                    state: location.address.state,
+                    postcode: location.address.postcode,
+                    country: location.address.country,
                   },
                 },
                 {
@@ -64,9 +71,11 @@ class Home extends Component {
     console.log(this.state.location)
     return (
       <div className="content-container">
-        <h1>Heatmap Page</h1>
+        <h1>Hello, {firebase.auth().currentUser.displayName}</h1>
         <p>Thdddlay the heatmap of covid-19</p>
         <p>{this.state.location.road}</p>
+        <p>{this.state.lat}</p>
+        <Map lat={this.state.lat} lng={this.state.lng} />
       </div>
     )
   }
---
+++ src/components/home/Map.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef } from "react"
+
+const GOOGLE_MAP_API_KEY = "AIzaSyBcAUk21V9tUi3ZyziIG6TRirD3Uw_ECGM"
+const mapStyles = {
+  width: "800px",
+  height: "500px",
+}
+
+function Map(props) {
+  const googleMapRef = React.createRef()
+  const googleMap = useRef(null)
+  const marker = useRef(null)
+
+  const createGoogleMap = () =>
+    new window.google.maps.Map(googleMapRef.current, {
+      mapTypeControl: false,
+      zoom: 15,
+      center: {
+        lat: props.lat,
+        lng: props.lng,
+      },
+      styles: [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }],
+        },
+      ],
+    })
+
+  const createMarker = () =>
+    new window.google.maps.Marker({
+      position: { lat: props.lat, lng: props.lng },
+      map: googleMap.current,
+    })
+
+  useEffect(() => {
+    const googleMapScript = document.createElement("script")
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`
+    window.document.body.appendChild(googleMapScript)
+
+    googleMapScript.addEventListener("load", () => {
+      googleMap.current = createGoogleMap()
+      marker.current = createMarker()
+    })
+  })
+
+  return <div id="google-map" ref={googleMapRef} style={mapStyles} />
+}
+
+export default Map
