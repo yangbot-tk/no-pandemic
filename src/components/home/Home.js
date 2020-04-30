621624b2d31d3794d669aa07306f89bdceab72f7
yang052413 <liyang0525.ly@gmail.com>
2020-04-30 16:06:27 -0700

    latest stable version

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
-import MyMap from './MyMap';
-import Loading from '../Loading';
+import React, { Component } from "react"
+import firebase from "firebase"
+import MyMap from "./MyMap"
+import Loading from "../Loading"
 
 export default function Home() {
   return (
@@ -9,8 +9,8 @@ export default function Home() {
       <h1>Home Dashboard</h1>
       <div className="content-container">
         {/* <h2>Hello, {firebase.auth().currentUser.displayName}</h2> */}
-        <MyMap />
+        {/* <MyMap /> */}
       </div>
     </div>
-  );
+  )
 }
--- src/components/home/MyMap.js
+++ src/components/home/MyMap.js
@@ -1,84 +1,84 @@
-import React, { useState, useRef } from 'react';
-import GoogleMapReact from 'google-map-react';
-import trees from '../../data/street-treesgeo.json';
-import useSupercluster from 'use-supercluster';
-import { usePosition } from 'use-position';
-import '../../style/MyMap.css';
+import React, { useState, useRef } from "react"
+import GoogleMapReact from "google-map-react"
+import trees from "../../data/street-treesgeo.json"
+import useSupercluster from "use-supercluster"
+import { usePosition } from "use-position"
+import "../../style/MyMap.css"
 {
   /* Please create a .env file in root folder
   copy the code below and replace your api key
-  REACT_APP_GOOGLE_API_KEY='YOUR_API_KEY_HERE'
+  REACT_APP_GOOGLE_API_KEY='AIzaSyBcAUk21V9tUi3ZyziIG6TRirD3Uw_ECGM'
 */
 }
 export default function MyMap() {
-  const Marker = ({ children }) => children;
-  const mapRef = useRef();
-  const [bounds, setBounds] = useState(null);
-  const [zoom, setZoom] = useState(10);
-  const userLocation = usePosition();
+  const Marker = ({ children }) => children
+  const mapRef = useRef()
+  const [bounds, setBounds] = useState(null)
+  const [zoom, setZoom] = useState(14)
+  const userLocation = usePosition()
 
   const style = {
-    height: '100vh',
-    width: '100%',
-  };
-  const treesdata = trees.features.filter((tree) => tree.geometry !== null);
+    height: "80vh",
+    width: "100%",
+  }
+  const treesdata = trees.features.filter((tree) => tree.geometry !== null)
   const points = treesdata.map((tree) => ({
-    type: 'Feature',
+    type: "Feature",
     properties: {
       cluster: false,
       treeId: tree.properties.tree_id,
       category: tree.properties.genus_name,
     },
     geometry: {
-      type: 'Point',
+      type: "Point",
       coordinates: [
         parseFloat(tree.geometry.coordinates[0]),
         parseFloat(tree.geometry.coordinates[1]),
       ],
     },
-  }));
+  }))
   const { clusters, supercluster } = useSupercluster({
     points,
     bounds,
     zoom,
     options: { radius: 75, maxZoom: 20 },
-  });
+  })
   const test = {
     lat: 49.278752,
     lng: -123.100365,
-  };
+  }
   if (userLocation.longitude)
     return (
       <div style={style}>
         <GoogleMapReact
           bootstrapURLKeys={{
-            key: process.env.REACT_APP_GOOGLE_API_KEY,
+            key: "AIzaSyBcAUk21V9tUi3ZyziIG6TRirD3Uw_ECGM",
           }}
           defaultCenter={{
             lat: userLocation.latitude,
             lng: userLocation.longitude,
           }}
-          defaultZoom={10}
+          defaultZoom={14}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map }) => {
-            mapRef.current = map;
+            mapRef.current = map
           }}
           onChange={({ zoom, bounds }) => {
-            setZoom(zoom);
+            setZoom(zoom)
             setBounds([
               bounds.nw.lng,
               bounds.se.lat,
               bounds.se.lng,
               bounds.nw.lat,
-            ]);
+            ])
           }}
         >
           {clusters.map((cluster) => {
-            const [longitude, latitude] = cluster.geometry.coordinates;
+            const [longitude, latitude] = cluster.geometry.coordinates
             const {
               cluster: isCluster,
               point_count: pointCount,
-            } = cluster.properties;
+            } = cluster.properties
 
             if (isCluster) {
               return (
@@ -98,18 +98,18 @@ export default function MyMap() {
                       const expansionZoom = Math.min(
                         supercluster.getClusterExpansionZoom(cluster.id),
                         20
-                      );
-                      mapRef.current.setZoom(expansionZoom);
+                      )
+                      mapRef.current.setZoom(expansionZoom)
                       mapRef.current.panTo({
                         lat: latitude,
                         lng: longitude,
-                      });
+                      })
                     }}
                   >
                     {pointCount}
                   </div>
                 </Marker>
-              );
+              )
             }
             //   return (
             //     <Marker
@@ -135,6 +135,6 @@ export default function MyMap() {
           </Marker>
         </GoogleMapReact>
       </div>
-    );
-  else return <div>loading</div>;
+    )
+  else return <div>loading</div>
 }
