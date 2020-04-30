ee4fdc4dc1a3c43fbaaa2e2d53b9dd333aa58e48
yang052413 <liyang0525.ly@gmail.com>
2020-04-29 18:10:19 -0700

    recommendation component restyling

--- src/components/aid/aiditem/Header.js
+++ src/components/aid/aiditem/Header.js
@@ -3,12 +3,14 @@ import React from "react"
 function Header() {
   return (
     <div className="aid-header-container">
-      <h2>Dear Yang Li</h2>
-      <p>
-        We know this is a hard to experience, and we provided the following
-        resource to help you to go through it
-      </p>
-      <button>Read more</button>
+      <div>
+        <h2>Dear Yang Li</h2>
+        <p>
+          We know this is a hard to experience, and we provided the following
+          resource to help you to go through it
+        </p>
+      </div>
+      <img src="/images/aidbanner.png" alt="aid-banner" />
     </div>
   )
 }
