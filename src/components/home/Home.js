c0a71ef57c0343c11cab4f23ac93aea280ad79cb
yang052413 <liyang0525.ly@gmail.com>
2020-05-01 12:26:18 -0700

    open cluster map

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 // import firebase from "firebase"
-// import MyMap from "./MyMap"
-// import Loading from "../Loading"
+import MyMap from "./MyMap"
+import Loading from "../Loading"
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <h1>Home Dashboard</h1>
       <div className="content-container">
         {/* <h2>Hello, {firebase.auth().currentUser.displayName}</h2> */}
-        {/* <MyMap /> */}
+        <MyMap />
       </div>
     </div>
   )
