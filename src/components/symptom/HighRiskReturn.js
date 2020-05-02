8b66c87fec8d08758c0e4eb1a235901f1ae9e3b4
yang052413 <liyang0525.ly@gmail.com>
2020-05-01 19:21:40 -0700

    created transition component for each situation based on user symptom result

---
+++ src/components/symptom/HighRiskReturn.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function HighRiskReturn() {
+  return (
+    <div className="error-container">
+      <h2>Please Contract Your Doctor</h2>
+      <p>
+        Based on your symptom accessment, you are currently seen as high risk
+        for COVID-19, please contact your local doctor for booking an
+        appointment test
+      </p>
+    </div>
+  )
+}
+
+export default HighRiskReturn
---
+++ src/components/symptom/LowRiskReturn.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function LowRiskReturn() {
+  return (
+    <div className="error-container">
+      <h2>You already filled the symptom accessment</h2>
+      <p>
+        We already have your data, and we are accessed as low risk for this
+        epedemic. Do you want to uodate your symptom?
+      </p>
+    </div>
+  )
+}
+
+export default LowRiskReturn
