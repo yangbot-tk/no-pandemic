7610459ad4db802811bb51680456e9bd7103a3f6
yang052413 <liyang0525.ly@gmail.com>
2020-05-03 23:05:04 -0700

    status component interactive map done

--- src/components/status/CovMap.js
+++ src/components/status/CovMap.js
@@ -3,7 +3,12 @@ import React from "react"
 function CovMap() {
   return (
     <div className="covmap-container">
-      <img src="/images/map.png" alt="cov map" width="820px" height="auto" />
+      {/* <img src="/images/map.png" alt="cov map" width="820px" height="auto" /> */}
+      <iframe
+        src="https://experience.arcgis.com/experience/a6f23959a8b14bfa989e3cda29297ded"
+        scrolling="no"
+        frameborder="no"
+      ></iframe>
     </div>
   )
 }
