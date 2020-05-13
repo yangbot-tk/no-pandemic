c6dc4d8d87b42a096adb5b61fd6c7f7e26ce807d
yang052413 <liyang0525.ly@gmail.com>
2020-05-13 13:55:22 -0700

    added transition animation to changing game fxied flash bug

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -4,15 +4,71 @@ import GameThum from "./GameThum"
 import GameText from "./GameText"
 
 function Game() {
-  const [game, setGame] = useState("")
+  const [game, setGame] = useState({
+    id: "game-1",
+    title: "",
+    info: "",
+    teamname: "",
+    teammember: [],
+    bgimage: "",
+  })
 
   function over(event) {
-    console.log(`你好${id}`)
     const { id } = event.currentTarget
+    if (id === "game-1") {
+      setGame({
+        id: "game-1",
+        title: "Safe Zone1",
+        info:
+          "Banter Bar is a virtual pub environment to provide users a platform to chat via messages, and potentially voice and video. It will include pub games such as pool and darts as activities, allowing people to gather as they would at a pub but virtually.",
+        teamname: "BCIT NMSL",
+        teammember: ["Yang Li", "Wenbo Ji", "Ben Jones", "Ian Snider"],
+        bgimage: "/images/game/game1.jpg",
+      })
+    } else if (id === "game-2") {
+      setGame({
+        id: "game-2",
+        title: "Safe Zone2",
+        info:
+          "Banter Bar is a virtual pub environment to provide users a platform to chat via messages, and potentially voice and video. It will include pub games such as pool and darts as activities, allowing people to gather as they would at a pub but virtually.",
+        teamname: "BCIT NMSL",
+        teammember: ["Yang Li", "Wenbo Ji", "Ben Jones", "Ian Snider"],
+        bgimage: "/images/game/game2.jpg",
+      })
+    } else if (id === "game-3") {
+      setGame({
+        id: "game-3",
+        title: "Safe Zone3",
+        info:
+          "Banter Bar is a virtual pub environment to provide users a platform to chat via messages, and potentially voice and video. It will include pub games such as pool and darts as activities, allowing people to gather as they would at a pub but virtually.",
+        teamname: "BCIT NMSL",
+        teammember: ["Yang Li", "Wenbo Ji", "Ben Jones", "Ian Snider"],
+        bgimage: "/images/game/game3.jpg",
+      })
+    } else if (id === "game-4") {
+      setGame({
+        id: "game-4",
+        title: "Happify.exe4",
+        info:
+          "Banter Bar is a virtual pub environment to provide users a platform to chat via messages, and potentially voice and video. It will include pub games such as pool and darts as activities, allowing people to gather as they would at a pub but virtually.",
+        teamname: "BCIT NMSL",
+        teammember: ["Yang Li", "Wenbo Ji", "Ben Jones", "Ian Snider"],
+        bgimage: "/images/game/game4.jpg",
+      })
+    } else if (id === "game-5") {
+      setGame({
+        id: "game-5",
+        title: "Safe Zone5",
+        info:
+          "Banter Bar is a virtual pub environment to provide users a platform to chat via messages, and potentially voice and video. It will include pub games such as pool and darts as activities, allowing people to gather as they would at a pub but virtually.",
+        teamname: "BCIT NMSL",
+        teammember: ["Yang Li", "Wenbo Ji", "Ben Jones", "Ian Snider"],
+        bgimage: "/images/game/game5.jpg",
+      })
+    }
   }
 
   function leave(event) {
-    console.log(`再见${event.currentTarget.id}`)
     const { id } = event.currentTarget
   }
 
@@ -20,7 +76,41 @@ function Game() {
     <div className="main-container">
       {/* <UserNav title="Game Dashboard" /> */}
 
-      <div className="game-container">
+      <div
+        // style={{ backgroundImage: "url(" + game.bgimage + ")" }}
+        className="game-container"
+      >
+        <img
+          style={game.id === "game-5" ? { opacity: 1 } : { opacity: 0 }}
+          className="game-bg"
+          id="game-5-bg"
+          src="/images/game/game5.jpg"
+        />
+        <img
+          style={game.id === "game-4" ? { opacity: 1 } : { opacity: 0 }}
+          className="game-bg"
+          id="game-4-bg"
+          src="/images/game/game4.jpg"
+        />
+        <img
+          style={game.id === "game-3" ? { opacity: 1 } : { opacity: 0 }}
+          className="game-bg"
+          id="game-3-bg"
+          src="/images/game/game3.jpg"
+        />
+        <img
+          style={game.id === "game-2" ? { opacity: 1 } : { opacity: 0 }}
+          className="game-bg"
+          id="game-2-bg"
+          src="/images/game/game2.jpg"
+        />
+        <img
+          style={game.id === "game-1" ? { opacity: 1 } : { opacity: 0 }}
+          className="game-bg"
+          id="game-1-bg"
+          src="/images/game/game1.jpg"
+        />
+
         <div className="game-item-container">
           <div className="game-item-thum">
             <div id="game-1" onMouseEnter={over} onMouseLeave={leave}>
@@ -40,10 +130,10 @@ function Game() {
             </div>
           </div>
           <GameText
-            title="Counter Strike: Global Offensive"
-            info="Banter Bar is a virtual pub environment to provide users a platform to chat via messages, and potentially voice and video. It will include pub games such as pool and darts as activities, allowing people to gather as they would at a pub but virtually."
-            teamName="BCIT NMSL"
-            teamMember={["Yang Li", "Wenbo Ji", "Ben Jones", "Ian Snider"]}
+            title={game.title}
+            info={game.info}
+            teamName={game.teamname}
+            teamMember={game.teammember}
           />
         </div>
       </div>
