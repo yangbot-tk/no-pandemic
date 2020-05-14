566ff9acfbaf4b55e508fd9dc0337e4194d78a11
yang052413 <liyang0525.ly@gmail.com>
2020-05-14 14:12:57 -0700

    fixed profile picture display issue and added overlay to top

--- src/components/profile/Banner.js
+++ src/components/profile/Banner.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import firebase from "firebase"
 import $ from "jquery"
 
@@ -7,31 +7,46 @@ function Banner(props) {
     const db = firebase.firestore()
     const storageRef = firebase.storage().ref()
     let file = $("#profile-input").prop("files")[0]
-    let metadata = {
-      contentType: file.type,
-    }
+    if (file) {
+      let metadata = {
+        contentType: file.type,
+      }
 
-    let task = storageRef.child(file.name).put(file, metadata)
-    task
-      .then((snapshot) => snapshot.ref.getDownloadURL())
-      .then((url) => {
-        console.log("用户头像上传成功 链接为: " + url)
+      let task = storageRef.child(file.name).put(file, metadata)
+      task
+        .then((snapshot) => snapshot.ref.getDownloadURL())
+        .then((url) => {
+          console.log("用户头像上传成功 链接为: " + url)
 
-        firebase.auth().onAuthStateChanged((user) => {
-          db.collection("user").doc(user.uid).update({
-            Profile: url,
+          firebase.auth().onAuthStateChanged((user) => {
+            db.collection("user").doc(user.uid).update({
+              Profile: url,
+            })
           })
         })
-      })
+    } else {
+      alert("!!!!")
+    }
   }
   return (
     <div className="profile-banner">
       <div className="profile-user">
-        <img src={props.profileUrl} alt="user-profile" />
+        <div className="profile-image">
+          <div className="profile-overlay"></div>
+          <img src={props.profileUrl} alt="user-profile" />
+        </div>
         <h3>{props.name}</h3>
       </div>
-      <input id="profile-input" type="file" />
-      <button onClick={handleImg}>Upload</button>
+
+      <div className="profile-upload">
+        <input id="profile-input" name="profile-input" type="file" />
+
+        <label for="profile-input">
+          <img src="/images/camera.png" alt="change pic" />
+        </label>
+
+        <button onClick={handleImg}>Upload</button>
+      </div>
     </div>
   )
 }
